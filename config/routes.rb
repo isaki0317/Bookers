203e4a270b3fc9c1974c0684cc9520d10e7e1776
isaki0317 <matsuoisaki.0317@gmail.com>
2020-10-05 13:53:12 +0000

    [update]

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get 'books/index'
-  get 'books/top'
-  get 'books/show'
-  get 'books/edit'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :books
+
+  root to: 'books#top'
+  # post 'books' => 'books#create'
+  # get 'show' => 'books#show', as: 'boooook'
 end
